f37ac36f7f3c0805aa8d2502099f3a194f86368e
Adrian_034 <adrianmesasacasas@outlook.com>
2023-04-18 18:10:34 -0400

    first commit

---
+++ Models/NavBarItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RulerHubLibrary.Models
+{
+    public partial class NavBarItems
+    {
+        public int Id { get; set; }
+        public string Label { get; set; } = null!;
+        public string OiItem { get; set; } = null!;
+        public string Ref { get; set; } = null!; 
+    }
+}
